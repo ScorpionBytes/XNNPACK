60732c18ea459395992a18a1ccd2036622fe278c
Dillon Sharlet <dsharlet@google.com>
2025-05-20 22:55:53 -0700

    Fix batch size of blockwise quantization scales for validation
    PiperOrigin-RevId: 761376343

--- src/tensor.c
+++ src/tensor.c
@@ -530,7 +530,11 @@ enum xnn_status xnn_define_blockwise_quantized_tensor_value_v2(
         xnn_datatype_to_string(scale_type), datatype);
       return xnn_status_unsupported_parameter;
   }
-  const size_t block_count = dims[0] * dims[1] / block_size;
+  size_t batch_size = 1;
+  for (size_t i = 0; i < num_dims; i++) {
+    batch_size *= dims[i];
+  }
+  const size_t block_count = batch_size / block_size;
   for (size_t block = 0; block < block_count; block++) {
     float float_scale;
     switch (scale_type) {
@@ -545,9 +549,9 @@ enum xnn_status xnn_define_blockwise_quantized_tensor_value_v2(
     }
     if (float_scale <= 0.0f || !isnormal(float_scale)) {
       xnn_log_error(
-        "failed to create Blockwise Quantized Dense Tensor value with %.7g scale in block #%zu: "
+        "failed to create Blockwise Quantized Dense Tensor value with %.7g scale: "
         "scale must be finite, normalized, and positive",
-        float_scale, block);
+        float_scale);
       return xnn_status_invalid_parameter;
     }
   }
